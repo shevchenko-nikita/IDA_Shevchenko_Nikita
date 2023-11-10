ce184b07d25d1244336691f0ec59dbe58c778b02
shevchenko-nikita <nicksevchenkos@gmail.com>
2023-11-10 14:02:56 +0200

    Lab_1

--- Labs/LAB_1/Lab_1.py
+++ Labs/LAB_1/Lab_1.py
@@ -3,6 +3,10 @@ import seaborn as sns
 import matplotlib
 def get_top(_lst, _gender, _age, _movies_df):
     top_movies = _lst[(_lst['Age'] == _age) & (_lst['Gender'] == _gender)]
+    rating_count = top_movies.groupby(['MovieID'])['Rating'].count()
+    rating_count.name = 'Rating Count'
+    top_movies = pd.merge(top_movies, rating_count, on='MovieID')
+    top_movies = top_movies[top_movies['Rating Count'] >= 20]
     top_movies = top_movies.groupby('MovieID')['Rating'].mean()
     top_movies = pd.merge(_movies_df, top_movies, on='MovieID')
     top_movies = top_movies.sort_values(by=['Rating'], ascending=[False])
@@ -24,7 +28,7 @@ users_df = pd.read_csv('LAB_1/users.dat', sep='::', names=['UserID', 'Gender', '
 merged_df = pd.merge(movies_df, ratings_df, on='MovieID')
 merged_df = pd.merge(merged_df, users_df, on='UserID')
 
-print_top_ten(merged_df, 'M', 56, movies_df)
+print_top_ten(merged_df, 'F', 1, movies_df)
 
 #show_top_ten(merged_df, 'M', 1, movies_df)
 
